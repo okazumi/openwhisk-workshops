23f53bb1f75c5d882b58daa95e650eee12c18f82
okazumi <k_ono@fts-dev.co.jp>
2018-02-06 22:20:45 +0900

    [#1][change]typoを修正

--- weather-bot/location_to_latlong.js
+++ weather-bot/location_to_latlong.js
@@ -2,24 +2,24 @@ var request = require("request");
 
 function main(params) {
     var options = {
-        url: "https://maps.googleapis.com/api/geocode/json",
+        url: "https://maps.googleapis.com/maps/api/geocode/json",
         qs: {address: params.text},
         json: true
     };
 
-    return new Promise(function(resolve, reject) {
-        request(options, function(err, resp) {
-            if(err) {
+    return new Promise(function (resolve, reject) {
+        request(options, function (err, resp) {
+            if (err) {
                 console.log(err);
-                return reject({err: resp});
+                return reject({err: err});
             }
 
-            if(resp.body.status !== "OK") {
+            if (resp.body.status !== "OK") {
                 console.log(resp.body.status);
                 return reject({err: resp.body.status});
             }
 
-            resolve(resp.body.results[0].geometry.locationi);
+            resolve(resp.body.results[0].geometry.location);
         });
     });
 }
